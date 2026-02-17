8bf4140b706d17c1809772b9e6bfdf143ee95b67
ayushgautamm <gautamayush7269066117@gmail.com>
2026-02-17 15:40:39 +0530

    Initial commit

---
+++ src/main/java/com/example/uberprojectentityservice/models/BookingStatus.java
@@ -0,0 +1,15 @@
+package com.example.uberprojectentityservice.models;
+
+import jakarta.persistence.Table;
+
+@Table
+public enum BookingStatus {
+
+    //TODO [Reverse Engineering] generate columns from DB
+    SCHEDULED,
+    CANCELED,
+    CAR_ARRIVED,
+    ASSIGNING_DRIVER,
+    IN_RIDE,
+    COMPLETED
+}
